a43cd3726b6be4cee4fa8ff87176d996f32a611c
Luyao787 <zly9706@gmail.com>
2025-10-03 16:05:39 +0200

    Major: add parallel solver

--- examples/simple_example.cpp
+++ examples/simple_example.cpp
@@ -6,6 +6,10 @@ using namespace lqr;
 int main() {
     std::cout << "PDP-LQR Library Example" << std::endl;
     
+    /*
+    * Example: A quadrotor example adapted from https://osqp.org/docs/release-0.6.3/examples/mpc.html
+    */
+
     constexpr int nx = 12;
     constexpr int nu = 4;
     constexpr int N  = 10;
@@ -16,10 +20,14 @@ int main() {
     x_ref << 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0.;
 
     VectorXs x_min(nx), x_max(nx), u_min(nu), u_max(nu);
-    x_min << -0.52359878, -0.52359878, -LQR_INFTY, -LQR_INFTY, -LQR_INFTY, -1.,
-        -LQR_INFTY, -LQR_INFTY, -LQR_INFTY, -LQR_INFTY, -LQR_INFTY, -LQR_INFTY;
-    x_max << 0.52359878, 0.52359878, LQR_INFTY, LQR_INFTY, LQR_INFTY, LQR_INFTY,
-        LQR_INFTY, LQR_INFTY, 2.5, LQR_INFTY, LQR_INFTY, LQR_INFTY;
+    x_min << -0.52359878, -0.52359878, -LQR_INFTY, 
+        -LQR_INFTY, -LQR_INFTY, -1.,
+        -LQR_INFTY, -LQR_INFTY, -LQR_INFTY, 
+        -LQR_INFTY, -LQR_INFTY, -LQR_INFTY;
+    x_max << 0.52359878, 0.52359878, LQR_INFTY, 
+        LQR_INFTY, LQR_INFTY, LQR_INFTY,
+        LQR_INFTY, LQR_INFTY, 2.5, 
+        LQR_INFTY, LQR_INFTY, LQR_INFTY;
     u_min << -0.9916, -0.9916, -0.9916, -0.9916;
     u_max << 2.4084, 2.4084, 2.4084, 2.4084;
    
@@ -71,74 +79,61 @@ int main() {
     q = -x_ref.transpose() * Q;
     r.setZero();
 
-    LQRModel lqr_model;
     LQRSettings lqr_settings;
     lqr_settings.max_iter = 1000;
     
-    lqr_model.n = nx;
-    lqr_model.m = nu;
-    lqr_model.N = N; 
+    LQRModel lqr_model(nx, nu, N);
 
     std::vector<int> ncs(N + 1);
     for (int k = 0; k < N; ++k) {
         int nc = nu + nx;
         ncs[k] = nc;
-        lqr_model.knotpoints.emplace_back(nx, nu, nc, k);
-        lqr_model.knotpoints[k].E << B, A;
-        lqr_model.knotpoints[k].c = c;
-        
-        lqr_model.knotpoints[k].H.block(0, 0, nu, nu) = R;
-        lqr_model.knotpoints[k].H.block(nu, nu, nx, nx) = Q;
-        lqr_model.knotpoints[k].H.block(0, nu, nu, nx) = S;
-        lqr_model.knotpoints[k].H.block(nu, 0, nx, nu) = S.transpose();       
-        lqr_model.knotpoints[k].h.head(nu) = r;
-        lqr_model.knotpoints[k].h.tail(nx) = q;
+        lqr_model.add_knotpoint(nx, nu, nc, k);
+        auto& kpoint = lqr_model.knotpoints[k];
 
-        lqr_model.knotpoints[k].D_con.setZero();
-        lqr_model.knotpoints[k].D_con.topLeftCorner(nu, nu).setIdentity();
+        kpoint.E << B, A;
+        kpoint.c = c;
+
+        kpoint.H.block(0, 0, nu, nu) = R;
+        kpoint.H.block(nu, nu, nx, nx) = Q;
+        kpoint.H.block(0, nu, nu, nx) = S;
+        kpoint.H.block(nu, 0, nx, nu) = S.transpose();       
+        kpoint.h.head(nu) = r;
+        kpoint.h.tail(nx) = q;
+
+        kpoint.D_con.setZero();
+        kpoint.D_con.topLeftCorner(nu, nu).setIdentity();
         if (k > 0) {
-            lqr_model.knotpoints[k].D_con.bottomRightCorner(nx, nx).setIdentity();
+            kpoint.D_con.bottomRightCorner(nx, nx).setIdentity();
         }
-        lqr_model.knotpoints[k].e_lb << u_min, x_min;
-        lqr_model.knotpoints[k].e_ub << u_max, x_max;          
+        kpoint.e_lb << u_min, x_min;
+        kpoint.e_ub << u_max, x_max;          
     }
     int nc = nx;
     ncs[N] = nc;
-    lqr_model.knotpoints.emplace_back(nx, nu, nc, N, true);
-    lqr_model.knotpoints[N].H = Q;
-    lqr_model.knotpoints[N].h = q;
-    lqr_model.knotpoints[N].D_con.setIdentity();
-    lqr_model.knotpoints[N].e_lb = x_min;
-    lqr_model.knotpoints[N].e_ub = x_max;
+    lqr_model.add_knotpoint(nx, nu, nc, N, true);
+    auto& kpoint = lqr_model.knotpoints[N];
+    kpoint.H = Q;
+    kpoint.h = q;
+    kpoint.D_con.setIdentity();
+    kpoint.e_lb = x_min;
+    kpoint.e_ub = x_max;
+
+    LQRResults lqr_results_serial, lqr_results_parallel;
+    lqr_results_serial.reset(nx, nu, ncs, N);
+    lqr_results_parallel.reset(nx, nu, ncs, N);
+
+    SerialCLQRSolver clqr_solver_serial(lqr_model);
+    ParallelCLQRSolver clqr_solver_parallel(lqr_model, 2); // Using 2 threads
+
+    clqr_solver_serial.solve(x0, lqr_settings, lqr_results_serial);
+    clqr_solver_parallel.solve(x0, lqr_settings, lqr_results_parallel);
+
+
+    std::cout << "Final state: " << lqr_results_serial.xs[N].transpose() << std::endl;
+    // Control inputs
+    for (int k = 0; k < std::min(5, N); ++k) {
+        std::cout << "Control input at step " << k << ": " << lqr_results_serial.us[k].transpose() << std::endl;
+    }
     
-    LQRResults lqr_results;
-    lqr_results.reset(nx, nu, ncs, N);
-
-    SerialCLQRSolver clqr_solver(lqr_model);
-
-    // ParallelCLQRSolver clqr_solver(lqr_model, 1);
-    // ParallelCLQRSolver clqr_solver(lqr_model, 2);
-
-    clqr_solver.solve(x0, lqr_settings, lqr_results);
-
-    auto tic0 = omp_get_wtime();
-    clqr_solver.clear_workspace();
-    clqr_solver.solve(x0, lqr_settings, lqr_results);
-    auto toc1 = omp_get_wtime();
-    std::cout << "Time for second solve call: " << (toc1 - tic0) * 1e3 << " ms" << "\n" << std::endl;
-
-    // std::cout << "Final state: " << lqr_results.xs[N].transpose() << std::endl;
-    // // Control inputs
-    // for (int k = 0; k < N; ++k) {
-    //     std::cout << "Control input at step " << k << ": " << lqr_results.us[k].transpose() << std::endl;
-    // }
-
-    // for (int k = 0; k <= N; ++k) {
-    //     std::cout << "State at step " << k << ": " << lqr_results.xs[k].transpose() << std::endl;
-    // }
-
-    // std::cout << "Dual variables (lambdas):" << std::endl;
-    // for (int k = 0; k <= N; ++k) {
-    //     std::cout << "Lambda at step " << k << ": " << lqr_results.lambdas[k].transpose() << std::endl;
-    // } 
 }
